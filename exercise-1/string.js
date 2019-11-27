33e40f33d84b9ef19906dc03011c18f4af3dacf6
Maxime <emixam.huet@sfr.fr>
2019-11-27 11:55:19 +0100

    fix

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -38,7 +38,7 @@ function capitalize(str) {
 function camelCase(str) {
     if (typeof str !== "string" || str.length === 0) return "";
     for (let j = 0; j < str.length; j++) {
-        str = str.replace(/[^A-Za-z\\ ]/, " ");
+        str = str.replace(/[^A-Za-z0-9\\ ]/gi, " ");
     }
     const array = str.toLowerCase().split(" ");
     for (let i = 0; i < array.length;) {
@@ -47,48 +47,64 @@ function camelCase(str) {
     return array.join("");
 }
 
-// console.log(camelCase('test_test'));
-// console.log(camelCase('$Test T-est'));
-// console.log(camelCase('3est 3est'));
-// console.log(camelCase('reSt !eZf'));
-// console.log(camelCase(''));
-// console.log(camelCase(null));
-// console.log(camelCase({}));
+console.log(camelCase('test_test'));
+console.log(camelCase('$Test T-est'));
+console.log(camelCase('3est 3est'));
+console.log(camelCase('reSt !eZf'));
+console.log(camelCase(''));
+console.log(camelCase(null));
+console.log(camelCase({}));
 
 function snake_case(str) {
     if (typeof str !== "string" || str.length === 0) return "";
-    const array = str.toLowerCase().split(" ");
-
-    return array.join("_");
+    return str.toLowerCase().replace(/[^A-Za-z0-9]/gi, "_");
 }
 
 // console.log(snake_case('test test'));
 // console.log(snake_case('Test Test'));
 // console.log(snake_case('3est 3est'));
-// console.log(snake_case('reSt reZf'));
+// console.log(snake_case('r-St reZf'));
 // console.log(snake_case(''));
 // console.log(snake_case(null));
 // console.log(snake_case({}));
 
 function leet(str) {
     if (typeof str !== "string" || str.length === 0) return "";
-    let voyelle = ['a', 'e', 'i', 'o', 'u', 'y'];
-    let replace = ['4', '3', '1', '0', '(_)', '7'];
-    for (let j = 0; j < str.length; j++) {
-        for (let i = 0; i < voyelle.length; i++) {
-            str = str.replace(voyelle[i], replace[i]);
+    //
+    // let voyelle = ['a', 'e', 'i', 'o', 'u', 'y'];
+    // let replace = ['4', '3', '1', '0', '(_)', '7'];
+    // for (let j = 0; j < str.length; j++) {
+    //     for (let i = 0; i < voyelle.length; i++) {
+    //         str = str.replace(voyelle[i], replace[i]);
+    //     }
+    // }
+    // return str;
+
+    return str.replace(/[aeiouy]/gi,function (car) {
+        switch (car) {
+            case "a":case "A":
+                return 4;
+            case "e":case "E":
+                return 3;
+            case "i":case "I":
+                return 1;
+            case "o":case "O":
+                return 0;
+            case "u":case "U":
+                return "(_)";
+            case "y":case "Y":
+                return 7;
         }
-    }
-    return str;
+    })
 }
 
-// console.log(leet('anaconda'));
-// console.log(leet('Test Test'));
-// console.log(leet('3est 3est'));
-// console.log(leet('reSt reZf'));
-// console.log(leet(''));
-// console.log(leet(null));
-// console.log(leet({}));
+console.log(leet('anaconda'));
+console.log(leet('Test Test'));
+console.log(leet('3est 3est'));
+console.log(leet('reSt reZf'));
+console.log(leet(''));
+console.log(leet(null));
+console.log(leet({}));
 
 function verlan(str) {
     if (typeof str !== "string" || str.length === 0) return "";
@@ -121,18 +137,18 @@ function yoda(str) {
 // console.log(yoda({}));
 
 function vig(str, key) {
-    if (typeof str !== "string" || typeof key !== "string" || str.length === 0 || key.length === 0 || str.length != key.length) return "";
-    for (let i = 0; i < str.length;i++) {
-        str[i] = str[i].charCodeAt(65+((str[i]-65)+((key[i]%key.length)-65))%26);
+    if (typeof str !== "string" || typeof key !== "string" || str.length === 0 || key.length === 0) return "";
+    const array = str.toLowerCase().split(" ");
+    for (let i = 0; i < array.length;i++) {
+        array[i] = array[i].charCodeAt(65+((array[i]-65)+((key[i]%key.length)-65))%26);
     }
-    return str;
+    return array;
 }
 
-console.log(vig('anticonstitutionnellement - foo'));
-console.log(vig('antiConstiTutioNnellement '));
-console.log(vig('une phrase tres tres longue mais qui ne veut absolument rien dire car c est juste un test - nawakdecheznawak'));
-console.log(vig('reSt reZf'));
-console.log(vig(' - test'));
-console.log(vig(null));
-console.log(vig({}));
+// console.log(vig('anticonstitutionnellement','foo'));
+// console.log(vig('antiConstiTutioNnellement ','foo'));
+// console.log(vig('une phrase tres tres longue mais qui ne veut absolument rien dire car c est juste un test','nawakdecheznawak'));
+// console.log(vig(' ','test'));
+// console.log(vig(null,'test'));
+// console.log(vig({},'test'));
 
